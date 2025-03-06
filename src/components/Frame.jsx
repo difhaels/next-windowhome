1b489be2aafb1f19c526ed017ce081145ed551a3
Agung Saputra <agungsaputradifh@gmail.com>
2025-03-06 21:50:00 +0700

    wa

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
-export default function Frame({children}) {
+import WhatsAppButton from "./WhatsAppButton";
+export default function Frame({ children }) {
   return (
     <div className="pt-14">
       <Navbar />
-        {children}
-      <Footer/>
+      {children}
+      <WhatsAppButton />
+      <Footer />
     </div>
-  )
+  );
 }
---
+++ src/components/WhatsAppButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/6281234567890" // Ganti dengan nomor WhatsApp kamu
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 sm:right-16 flex items-center justify-center bg-green-500 p-3 rounded-full shadow-lg hover:bg-green-600 transition-all"
+    >
+      <img src="/wa.svg" alt="WhatsApp" width={30} height={30} />
+    </a>
+  );
+}
