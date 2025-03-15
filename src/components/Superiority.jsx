fedbd79628d633f8a51987c27b7d112011717313
Agung Saputra <agungsaputradifh@gmail.com>
2025-03-15 22:47:24 +0700

    new s

--- src/components/Superiority.jsx
+++ src/components/Superiority.jsx
@@ -5,7 +5,10 @@ function Superiority() {
   return (
     <section className="pt-16">
       <Container>
-        <h3 data-aos="fade-up" className="text-center text-2xl font-semibold mb-6">
+        <h3
+          data-aos="fade-up"
+          className="text-center text-2xl font-semibold mb-6"
+        >
           Keunggulan Rumah Kusen Aluminium
         </h3>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
@@ -14,44 +17,67 @@ function Superiority() {
             data-aos="fade-up"
           >
             <img
-              src="/worker.svg"
-              alt="Worker"
-              className="mx-auto mb-3 w-12 h-12"
+              src="/teamwork.svg"
+              alt="Tim Profesional"
+              className="mx-auto  w-auto h-50"
             />
-            <p className="text-lg font-medium">Tim Profesional</p>
+            <p className="text-lg font-semibold text-gray-800">
+              Tim Profesional
+            </p>
+            <p className="text-sm text-gray-600 mt-2">
+              Tim berpengalaman dengan hasil pemasangan presisi, rapi, dan
+              berkualitas tinggi.
+            </p>
           </div>
+
           <div
             className="bg-white shadow-md p-4 text-center rounded-lg"
             data-aos="fade-up"
           >
             <img
-              src="/cheap.svg"
-              alt="Cheap"
-              className="mx-auto mb-3 w-12 h-12"
+              src="/bestprice.svg"
+              alt="Harga Terjangkau"
+              className="mx-auto  w-auto h-50"
             />
-            <p className="text-lg font-medium">Harga Terjangkau</p>
+            <p className="text-lg font-semibold text-gray-800">
+              Harga Terjangkau
+            </p>
+            <p className="text-sm text-gray-600 mt-2">
+              Dapatkan layanan berkualitas dengan harga yang bersaing dan
+              transparan.
+            </p>
           </div>
+
           <div
             className="bg-white shadow-md p-4 text-center rounded-lg"
             data-aos="fade-up"
           >
             <img
-              src="/fast.svg"
-              alt="Fast"
-              className="mx-auto mb-3 w-12 h-12"
+              src="/faster.svg"
+              alt="Proses Cepat"
+              className="mx-auto  w-auto h-50"
             />
-            <p className="text-lg font-medium">Proses Cepat</p>
+            <p className="text-lg font-semibold text-gray-800">Proses Cepat</p>
+            <p className="text-sm text-gray-600 mt-2">
+              Kami memastikan pemasangan dilakukan dengan efisiensi tanpa
+              mengurangi kualitas.
+            </p>
           </div>
+
           <div
             className="bg-white shadow-md p-4 text-center rounded-lg"
             data-aos="fade-up"
           >
             <img
               src="/guarantee.svg"
-              alt="Guarantee"
-              className="mx-auto mb-3 w-12 h-12"
+              alt="Bergaransi"
+              className="mx-auto  w-auto h-50"
             />
-            <p className="text-lg font-medium">Bergaransi</p>
+            <p className="text-lg font-semibold text-gray-800">Bergaransi</p>
+            <p className="text-sm text-gray-600 mt-2">
+              Jaminan kualitas terbaik dengan layanan purna jual yang dapat
+              diandalkan.
+            </p>
           </div>
         </div>
       </Container>
