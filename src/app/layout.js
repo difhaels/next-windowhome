1fb2ece47514e4d7112265d2bc8cc21d9fa7c0e2
Agung Saputra <agungsaputradifh@gmail.com>
2025-04-14 23:15:09 +0700

    gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,35 +53,34 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" href="/favicon-256x256.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        
+
         {/* Google Tag Manager */}
         <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-K4CKDJMB" 
-        />
-        <Script
-          id="google-analytics"
+          id="gtm-head"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'GTM-K4CKDJMB'); 
-            `,
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-K4CKDJMB');
+    `,
           }}
         />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <AOSProvider />
 
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K4CKDJMB" //
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4CKDJMB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
 
